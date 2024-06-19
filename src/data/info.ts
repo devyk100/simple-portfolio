5a71cc38387f3e73b6081c7a5aa9e945a489f4dc
yash <devyk1000@gmail.com>
2024-06-19 22:37:32 +0530

    aut complete almost

---
+++ src/data/info.ts
@@ -0,0 +1,67 @@
+export const info = {
+  baseUrl: "https://yashk.dev",
+  name: "Yash Kumar",
+  jobDescription: "Fullstack Developer",
+  about: `I'm a second year engineering student at NIT Warangal.
+   I enjoy creating things that live on the internet, whether that be websites, or web connected native applications, or anything in between. 
+  I have experience in ReactJS, React Native, Redux, ExpressJS and NextJS.
+  `,
+
+  experience: [
+    {
+      name: "Mobile app, backend and Frontend development internship",
+      location: "ICRISAT Organisation (Remote)",
+      startDate: "February 2024",
+      endDate: "June 2024",
+      description: [
+        "- Developed a React Native app from scratch for crops data collection using Redux, MMC storage, Axios, and Google Maps API. Also worked with image processing of the clicked photos locally, GPS, and the compass.",
+        "- Also developed the backend in ExpressJS, Prisma, PostgreSQL and handled automatic file generations in CSV, XLSX, and .SHP. "
+        , "- Frontend in React JS with Redux, Tailwind and Axios handling image uploads for making the dashboard for this data collection. ",
+      ],
+    },
+
+  ],
+
+  education: [
+    {
+      name: "National Institute of Technology Warangal",
+      location: "Warangal, Telengana, India",
+      startDate: "2023",
+      endDate: "2027",
+      description: [
+        "Bachelor of Technology ( Electronics and Communication Engineering )",
+      ],
+    },
+    {
+      name: "Nalanda Academy, Kota",
+      location: "Kota, Rajasthan, India",
+      startDate: "2023",
+      endDate: "2022",
+      description: ["12th Grade"],
+    },
+    {
+      name: "Jawahar Navodaya Vidyalaya Palghar",
+      location: "Palghar, Maharasthra, India",
+      startDate: "2016",
+      endDate: "2022",
+      description: ["10th Grade"],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/devyk100",
+    github: "https://github.com/devyk100",
+    email: "mailto:devyk100@gmail.com",
+    linkedin: "https://www.linkedin.com/in/devyk100/",
+  },
+
+  projects: [
+    // {
+    //   title: "Astro Portfolio",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    //   liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    // },
+  ],
+};
